8c1d33e367a9fdd546e34bb13891e58767b48dcd
lippoliv <oliver@lipperts-web.de>
2013-01-18 14:06:45 +0100

    Correct misspelled variable $Adress to $Address

--- test/test.class.address.php
+++ test/test.class.address.php
@@ -2,100 +2,100 @@
 	include_once '../core/libraries/Kimai/Classes/class.address.php';
 	
 	$errs = array();
-	$Adress = new Address();
+	$Address = new Address();
 	
-	$Adress->setCity("City");
-	$Adress->setContact("Contact");
-	$Adress->setFax("Fax");
-	$Adress->setMail("Mail");
-	$Adress->setPhone("Phone");
-	$Adress->setStreet("Street");
-	$Adress->setWeb("Web");
-	$Adress->setZipcode("Zip");
+	$Address->setCity("City");
+	$Address->setContact("Contact");
+	$Address->setFax("Fax");
+	$Address->setMail("Mail");
+	$Address->setPhone("Phone");
+	$Address->setStreet("Street");
+	$Address->setWeb("Web");
+	$Address->setZipcode("Zip");
 	
-	if($Adress->getCity() != "City"){
-		$errs[] = "getCity doesn't return expected Value ('City' vs. '". $Adress->getCity() ."')";
+	if($Address->getCity() != "City"){
+		$errs[] = "getCity doesn't return expected Value ('City' vs. '". $Address->getCity() ."')";
 	}
 	
-	if($Adress->getContact() != "Contact"){
-		$errs[] = "getContact doesn't return expected Value ('Contact' vs. '". $Adress->getContact() ."')";
+	if($Address->getContact() != "Contact"){
+		$errs[] = "getContact doesn't return expected Value ('Contact' vs. '". $Address->getContact() ."')";
 	}
 	
-	if($Adress->getStreet() != "Street"){
-		$errs[] = "getStreet doesn't return expected Value ('Street' vs. '". $Adress->getStreet() ."')";
+	if($Address->getStreet() != "Street"){
+		$errs[] = "getStreet doesn't return expected Value ('Street' vs. '". $Address->getStreet() ."')";
 	}
 	
-	if($Adress->getZipcode() != ""){
-		$errs[] = "getZipcode doesn't return expected Value ('' vs. '". $Adress->getZipcode() ."')";
+	if($Address->getZipcode() != ""){
+		$errs[] = "getZipcode doesn't return expected Value ('' vs. '". $Address->getZipcode() ."')";
 	} else {
-		$Adress->setZipcode("00001");
+		$Address->setZipcode("00001");
 
-		if($Adress->getZipcode() != "00001"){
-			$errs[] = "getZipcode doesn't return expected Value ('00001' vs. '". $Adress->getZipcode() ."')";
+		if($Address->getZipcode() != "00001"){
+			$errs[] = "getZipcode doesn't return expected Value ('00001' vs. '". $Address->getZipcode() ."')";
 		}
 	}
 	
-	if($Adress->getFax() != ""){
-		$errs[] = "getFax doesn't return expected Value ('' vs. '". $Adress->getFax() ."')";
+	if($Address->getFax() != ""){
+		$errs[] = "getFax doesn't return expected Value ('' vs. '". $Address->getFax() ."')";
 	} else {
-		$Adress->setFax("1234512345");
+		$Address->setFax("1234512345");
 		
-		if($Adress->getFax() != "1234512345"){
-			$errs[] = "getFax doesn't return expected Value ('1234512345' vs. '". $Adress->getFax() ."')";
+		if($Address->getFax() != "1234512345"){
+			$errs[] = "getFax doesn't return expected Value ('1234512345' vs. '". $Address->getFax() ."')";
 		} else {
-			$Adress->setFax("+49 2345 12345");
+			$Address->setFax("+49 2345 12345");
 			
-			if($Adress->getFax() != "+49 2345 12345"){
-				$errs[] = "getFax doesn't return expected Value ('+49 2345 12345' vs. '". $Adress->getFax() ."')";
+			if($Address->getFax() != "+49 2345 12345"){
+				$errs[] = "getFax doesn't return expected Value ('+49 2345 12345' vs. '". $Address->getFax() ."')";
 			}
 		}
 	}
 	
-	if($Adress->getPhone() != ""){
-		$errs[] = "getPhone doesn't return expected Value ('' vs. '". $Adress->getPhone() ."')";
+	if($Address->getPhone() != ""){
+		$errs[] = "getPhone doesn't return expected Value ('' vs. '". $Address->getPhone() ."')";
 	} else {
-		$Adress->setPhone("1234512345");
+		$Address->setPhone("1234512345");
 		
-		if($Adress->getPhone() != "1234512345"){
-			$errs[] = "getPhone doesn't return expected Value ('1234512345' vs. '". $Adress->getPhone() ."')";
+		if($Address->getPhone() != "1234512345"){
+			$errs[] = "getPhone doesn't return expected Value ('1234512345' vs. '". $Address->getPhone() ."')";
 		} else {
-			$Adress->setPhone("+49 2345 12345");
+			$Address->setPhone("+49 2345 12345");
 			
-			if($Adress->getPhone() != "+49 2345 12345"){
-				$errs[] = "getPhone doesn't return expected Value ('+49 2345 12345' vs. '". $Adress->getPhone() ."')";
+			if($Address->getPhone() != "+49 2345 12345"){
+				$errs[] = "getPhone doesn't return expected Value ('+49 2345 12345' vs. '". $Address->getPhone() ."')";
 			}
 		}
 	}
 	
-	if($Adress->getMail() != ""){
-		$errs[] = "getMail doesn't return expected Value ('' vs. '". $Adress->getMail() ."')";
+	if($Address->getMail() != ""){
+		$errs[] = "getMail doesn't return expected Value ('' vs. '". $Address->getMail() ."')";
 	} else {
-		$Adress->setMail("mail@mail.com");
+		$Address->setMail("mail@mail.com");
 		
-		if($Adress->getMail() != "mail@mail.com"){
-			$errs[] = "getMail doesn't return expected Value ('mail@mail.com' vs. '". $Adress->getMail() ."')";
+		if($Address->getMail() != "mail@mail.com"){
+			$errs[] = "getMail doesn't return expected Value ('mail@mail.com' vs. '". $Address->getMail() ."')";
 		}
 	}
 	
-	if($Adress->getWeb() != ""){
-		$errs[] = "getWeb doesn't return expected Value ('' vs. '". $Adress->getWeb() ."')";
+	if($Address->getWeb() != ""){
+		$errs[] = "getWeb doesn't return expected Value ('' vs. '". $Address->getWeb() ."')";
 	} else {
-		$Adress->setWeb("http://www.google.com");
+		$Address->setWeb("http://www.google.com");
 		
-		if($Adress->getWeb() != "http://www.google.com"){
-			$errs[] = "getWeb doesn't return expected Value ('http://www.google.com' vs. '". $Adress->getWeb() ."')";
+		if($Address->getWeb() != "http://www.google.com"){
+			$errs[] = "getWeb doesn't return expected Value ('http://www.google.com' vs. '". $Address->getWeb() ."')";
 		} else {
-			$Adress->setWeb("");
-			$Adress->setWeb("www.google.com");
+			$Address->setWeb("");
+			$Address->setWeb("www.google.com");
 			
-			if($Adress->getWeb() != "http://www.google.com"){
-				$errs[] = "getWeb doesn't return expected Value ('http://www.google.com' vs. '". $Adress->getWeb() ."')";
+			if($Address->getWeb() != "http://www.google.com"){
+				$errs[] = "getWeb doesn't return expected Value ('http://www.google.com' vs. '". $Address->getWeb() ."')";
 			} else {
-				$Adress->setWeb("");
-				$Adress->setWeb("google.com");
+				$Address->setWeb("");
+				$Address->setWeb("google.com");
 				
-				if($Adress->getWeb() != "http://www.google.com"){
-					$errs[] = "getWeb doesn't return expected Value ('http://www.google.com' vs. '". $Adress->getWeb() ."')";
+				if($Address->getWeb() != "http://www.google.com"){
+					$errs[] = "getWeb doesn't return expected Value ('http://www.google.com' vs. '". $Address->getWeb() ."')";
 				}
 			}
 		}
