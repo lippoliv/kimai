8cf23d7bd65ef95b896b2662cdf9a2e2a4383e31
lippoliv <oliver@lipperts-web.de>
2013-01-23 08:34:34 +0100

    Added <br> after showing an Object in dev.objects, so the amount of Objects is better visualized

--- core/templates/scripts/dev/objects.php
+++ core/templates/scripts/dev/objects.php
@@ -2,16 +2,16 @@
 	<?php global $Kimai; ?>
 	<h2>Customers</h2>
 	<?php foreach($Kimai->getCustomers() as $Customer): ?>
-		<?php echo $Customer->toString(); ?>
+		<?php echo $Customer->toString(); ?><br />
 	<?php endforeach; ?>
 	
 	<h2>Projects</h2>
 	<?php foreach($Kimai->getProjects() as $Project): ?>
-		<?php echo $Project->toString(); ?>
+		<?php echo $Project->toString(); ?><br />
 	<?php endforeach; ?>
 	
 	<h2>Activities</h2>
 	<?php foreach($Kimai->getActivities() as $Activity): ?>
-		<?php echo $Activity->toString(); ?>
+		<?php echo $Activity->toString(); ?><br />
 	<?php endforeach; ?>
 </div>
